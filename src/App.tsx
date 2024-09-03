48e54c5236f75dd94518769ec1713be6211b8c32
Kaustubh <kaustubhjoshir@gmail.com>
2024-09-03 20:32:14 +0530

    Added rudimentary Tailwind CSS and Redux logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Browse } from "./pages/Browse";
+import { NotFound } from "./pages/NotFound";
+import { Random } from "./pages/Random";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Routes>
+			<Route path="/*" element={<Home />} />
+			<Route path="/browse/*" element={< Browse/>} />
+			<Route path="/genres/*" element={< Browse/>} />
+			<Route path="/random/*" element={< Random/>} />
+			<Route path="/*" element={< NotFound/>} />
+		</Routes>
+	);
 }
-
-export default App;
